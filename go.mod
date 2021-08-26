6463ff4d9abc2f4ee66244d20c5d76bffa8fd12d
Christian Leo <cleo@veritran.com>
2021-08-26 19:49:39 -0300

    Adding support for create Row struct from external query

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.10
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.2.0
 	github.com/mattn/go-sqlite3 v1.14.6
 )
--- sqlx_external.go
+++ sqlx_external.go
@@ -5,3 +5,7 @@ import "database/sql"
 func RowsFromExternal(r *sql.Rows, db *DB) *Rows {
 	return &Rows{Rows: r, unsafe: db.unsafe, Mapper: db.Mapper}
 }
+
+func RowFromExternal(r *sql.Rows, err error, db *DB) *Row {
+	return &Row{rows: r, err: err, unsafe: db.unsafe, Mapper: db.Mapper}
+}
